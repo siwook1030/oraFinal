19be8cc311f35f2b0466f5645200367216fcf046
kmj5970 <kmj5970@DESKTOP-SEDBTR6>
2020-11-13 20:48:58 +0900

    11/13 찜코스(ajax 완료) 내 정보수정 (기능 완료)

--- oraFinal/src/main/java/com/example/demo/controller/CourseController.java
+++ oraFinal/src/main/java/com/example/demo/controller/CourseController.java
@@ -34,159 +34,168 @@ import com.google.gson.Gson;
 
 @Controller
 public class CourseController {
-	
-	
-	@Autowired
-	private CourseDao cdao;
-	
-	@GetMapping("/searchCourse")
-	public void searchCourse() {
-		
-	}
-	
-	@PostMapping(value ="/searchCourse", produces = "application/json; charset=utf8")
-	@ResponseBody
-	public String searchCourse(HttpSession session,double latitude, double longitude, int distance, int time,@RequestParam(value="view[]",required = false) List<String> view) {
-		HashMap map = new HashMap();
-		System.out.println("위도 : "+latitude);
-		System.out.println("경도 : "+longitude);
-		System.out.println("거리 : "+distance);
-		System.out.println("시간 : "+time);
-		int minDis = 0;
-		if(distance > 0) { 
-			 minDis = 20;
-		}
-		if(distance >50) {
-			minDis = 950;
-		}
-		
-		int minTime = 0;
-		if(time > 0) {
-			 minTime = 60;
-		}
-		if(time > 180) {
-			minTime = 820;
-		}
-		
-		
-		if(view != null) {
-			int cnt=1;
-			for(String v : view) {
-				map.put("view"+cnt, v);
-				cnt++;
-			}
-		}
-			
-		map.put("latitude", latitude);
-		map.put("longitude", longitude);
-		map.put("distance", distance);
-		map.put("minDis", minDis);
-		map.put("time", time);
-		map.put("minTime", minTime);
-		List<CourseVo> sclList = cdao.searchCourseList(map);
-		
-		Gson gson = new Gson();
-		
-		return gson.toJson(sclList);
-		
-	}
-	
-	
-	@RequestMapping(value = "/detailCourse", produces = "application/json; charset=utf-8")
-	public void detailCourse(HttpServletRequest request,Model model, int c_no) {
-		String path = request.getRealPath("/courseLine")+"/";
-		Gson gson = new Gson();
-		CourseVo c = cdao.getCourseByCno(c_no, path);
-		List<PublicTransportVo> ptList = cdao.getPublicTransportByCno(c_no);
-	//	List<FoodVo> fList = cdao.getFoodByCno(c_no);
-		model.addAttribute("c", c);
-		model.addAttribute("cJson", gson.toJson(c));
-		model.addAttribute("ptList", ptList);
-		model.addAttribute("ptJson", gson.toJson(ptList));
-	//	model.addAttribute("fList", fList);
-	//	model.addAttribute("fJson", gson.toJson(fList));
-	}
-	@RequestMapping(value = "/admin/deleteCourse", produces = "application/json; charset=utf-8")
-	@ResponseBody
-	public String deleteCourse(HttpServletRequest request, int c_no) {
-		
-		 ResponseDataVo responseDataVo = new ResponseDataVo();
-		 responseDataVo.setCode(ResponseDataCode.ERROR);
-		 responseDataVo.setMessage("삭제에 실패하였습니다.");
-		 int re = -1;
-		 re = cdao.deleteCourse(c_no);
-		 String cLinepath = request.getRealPath(MakingCourseController.courseLinePath);
-		 String cLineName = c_no + MakingCourseController.courseLineName;
-		 String cPhotoFolderPath = request.getRealPath(MakingCourseController.coursePhotoPath)+MakingCourseController.coursePhotoPathSub+c_no;
-		 if( re > 0) {
-			 try {
-				 FileUtilCollection.deleteFile(cLinepath+"/"+cLineName);
-				 FileUtilCollection.deleteFolder(cPhotoFolderPath);
-			 }catch (Exception e) {
-				System.out.println("딜리트코스 파일예외 " + e.getMessage());
-			}	 
-			 responseDataVo.setCode(ResponseDataCode.SUCCESS);
-			 responseDataVo.setMessage("삭제에 성공하였습니다");
-		 }
-		 	 
-		 Gson gson = new Gson();
-		 return gson.toJson(responseDataVo);
-	}
-	
+   
+   
+   @Autowired
+   private CourseDao cdao;
+   
+   @GetMapping("/searchCourse")
+   public void searchCourse() {
+      
+   }
+   
+   @PostMapping(value ="/searchCourse", produces = "application/json; charset=utf8")
+   @ResponseBody
+   public String searchCourse(HttpSession session,double latitude, double longitude, int distance, int time,@RequestParam(value="view[]",required = false) List<String> view) {
+      HashMap map = new HashMap();
+      System.out.println("위도 : "+latitude);
+      System.out.println("경도 : "+longitude);
+      System.out.println("거리 : "+distance);
+      System.out.println("시간 : "+time);
+      int minDis = 0;
+      if(distance > 0) { 
+          minDis = 20;
+      }
+      if(distance >50) {
+         minDis = 950;
+      }
+      
+      int minTime = 0;
+      if(time > 0) {
+          minTime = 60;
+      }
+      if(time > 180) {
+         minTime = 820;
+      }
+      
+      
+      if(view != null) {
+         int cnt=1;
+         for(String v : view) {
+            map.put("view"+cnt, v);
+            cnt++;
+         }
+      }
+         
+      map.put("latitude", latitude);
+      map.put("longitude", longitude);
+      map.put("distance", distance);
+      map.put("minDis", minDis);
+      map.put("time", time);
+      map.put("minTime", minTime);
+      List<CourseVo> sclList = cdao.searchCourseList(map);
+      
+      Gson gson = new Gson();
+      
+      return gson.toJson(sclList);
+      
+   }
+   
+   
+   @RequestMapping(value = "/detailCourse", produces = "application/json; charset=utf-8")
+   public void detailCourse(HttpServletRequest request,Model model, int c_no) {
+      String path = request.getRealPath("/courseLine")+"/";
+      Gson gson = new Gson();
+      CourseVo c = cdao.getCourseByCno(c_no, path);
+      List<PublicTransportVo> ptList = cdao.getPublicTransportByCno(c_no);
+   //   List<FoodVo> fList = cdao.getFoodByCno(c_no);
+      model.addAttribute("c", c);
+      model.addAttribute("cJson", gson.toJson(c));
+      model.addAttribute("ptList", ptList);
+      model.addAttribute("ptJson", gson.toJson(ptList));
+   //   model.addAttribute("fList", fList);
+   //   model.addAttribute("fJson", gson.toJson(fList));
+   }
+   @RequestMapping(value = "/admin/deleteCourse", produces = "application/json; charset=utf-8")
+   @ResponseBody
+   public String deleteCourse(HttpServletRequest request, int c_no) {
+      
+       ResponseDataVo responseDataVo = new ResponseDataVo();
+       responseDataVo.setCode(ResponseDataCode.ERROR);
+       responseDataVo.setMessage("삭제에 실패하였습니다.");
+       int re = -1;
+       re = cdao.deleteCourse(c_no);
+       String cLinepath = request.getRealPath(MakingCourseController.courseLinePath);
+       String cLineName = c_no + MakingCourseController.courseLineName;
+       String cPhotoFolderPath = request.getRealPath(MakingCourseController.coursePhotoPath)+MakingCourseController.coursePhotoPathSub+c_no;
+       if( re > 0) {
+          try {
+             FileUtilCollection.deleteFile(cLinepath+"/"+cLineName);
+             FileUtilCollection.deleteFolder(cPhotoFolderPath);
+          }catch (Exception e) {
+            System.out.println("딜리트코스 파일예외 " + e.getMessage());
+         }    
+          responseDataVo.setCode(ResponseDataCode.SUCCESS);
+          responseDataVo.setMessage("삭제에 성공하였습니다");
+       }
+           
+       Gson gson = new Gson();
+       return gson.toJson(responseDataVo);
+   }
+   
 
-	@RequestMapping("/detailFood")
-	public void detailFood(HttpServletRequest request,Model model,int c_no ,int food_no) {
-		String path = request.getRealPath("/courseLine")+"/";
-		model.addAttribute("c", cdao.getCourseByCno(c_no, path));
-		model.addAttribute("f", cdao.getFoodByFoodNo(food_no));
-	}
-	
-	
-	//나의 찜코스 ~
-	@GetMapping(value = "/myPageSaveCourse")
-	public void saveCourse(Model model,HttpSession httpSession) {
-		System.out.println("세이브 컨트롤러 작동!!!!");
-		List<CourseVo> courseList = cdao.getSaveCourse(httpSession);
-		List<CoursePhotoVo> photovo =null;
-		for (CourseVo c : courseList) {
-			photovo =(c.getC_photo());
-		}
-		model.addAttribute("courseList",courseList);
-		model.addAttribute("photovo",photovo);
-		System.out.println(courseList);
-		System.out.println("코스리스트");
-		System.out.println(photovo);
-	}
-	//내가 만든코스~
-	@GetMapping(value = "/myPageMyCourse")
-	public void myCourse(Model model,HttpSession httpSession) {
-		System.out.println("마이코스 컨트롤러 작동!!!!");
-		List<CourseVo> courseList = cdao.getMyCourseById(httpSession);
-		List<CoursePhotoVo> photovo =null;
-		for (CourseVo c : courseList) {
-			photovo =(c.getC_photo());
-		}
-		model.addAttribute("courseList",courseList);
-		model.addAttribute("photovo",photovo);
-		System.out.println(courseList);
-		System.out.println("코스리스트");
-		System.out.println(photovo);
-	}
-	//찜코스 삭제~
-	@GetMapping(value = "/deleteSaveCourse")
-	public int deleteSaveCourse(HttpSession httpSession,int c_no) {
-		MemberVo m = (MemberVo)httpSession.getAttribute("m");
-		HashMap map = new HashMap();
-		map.put("id", m.getId());
-		map.put("c_no", c_no);
-		int re = cdao.deleteSaveCourse(map);
-		return re;
-	}
+   @RequestMapping("/detailFood")
+   public void detailFood(HttpServletRequest request,Model model,int c_no ,int food_no) {
+      String path = request.getRealPath("/courseLine")+"/";
+      model.addAttribute("c", cdao.getCourseByCno(c_no, path));
+      model.addAttribute("f", cdao.getFoodByFoodNo(food_no));
+   }
+   
+   @GetMapping(value = "/myPageSaveCourse")
+      public void saveCourse() {
+      }
+   
+   //나의 찜코스 ~
+   @PostMapping(value = "/myPageSaveCourse", produces = "application/json; charset=utf-8")
+   @ResponseBody
+   public String saveCourse(HttpSession httpSession) {
+      System.out.println("세이브 컨트롤러 작동!!!!");
+      Gson gson = new Gson();
+      List<CourseVo> courseList = cdao.getSaveCourse(httpSession);
+      List<CoursePhotoVo> photovo =null;
+      for (CourseVo c : courseList) {
+         photovo =(c.getC_photo());
+      }
+      //model.addAttribute("courseList",courseList);
+      //model.addAttribute("photovo",photovo);
+      System.out.println(courseList);
+      System.out.println("코스리스트");
+      System.out.println(photovo);
+      
+      return gson.toJson(courseList);
+   }
+   //내가 만든코스~
+   @GetMapping(value = "/myPageMyCourse")
+   public void myCourse(Model model,HttpSession httpSession) {
+      System.out.println("마이코스 컨트롤러 작동!!!!");
+      List<CourseVo> courseList = cdao.getMyCourseById(httpSession);
+      List<CoursePhotoVo> photovo =null;
+      for (CourseVo c : courseList) {
+         photovo =(c.getC_photo());
+      }
+      model.addAttribute("courseList",courseList);
+      model.addAttribute("photovo",photovo);
+      System.out.println(courseList);
+      System.out.println("코스리스트");
+      System.out.println(photovo);
+   }
+   //찜코스 삭제~
+   @PostMapping(value = "/deleteSaveCourse", produces = "application/json; charset=utf-8")
+   @ResponseBody
+   public String deleteSaveCourse(HttpSession httpSession,int cno) {
+      System.out.println("코스삭제 컨트롤러작동" + cno);
+      MemberVo m = (MemberVo)httpSession.getAttribute("m");
+      HashMap map = new HashMap();
+      map.put("id", m.getId());
+      map.put("c_no", cno);
+      String re = ""+cdao.deleteSaveCourse(map);
+      return re;
+   }
 
-//	@RequestMapping("/detailFood")  포기... crud가 너무 힘들다
-//	public void detailFood(HttpServletRequest request,Model model,int c_no ,int food_no) {
-//		String path = request.getRealPath("/courseLine")+"/";
-//		model.addAttribute("c", cdao.getCourseByCno(c_no, path));
-//		model.addAttribute("f", cdao.getFoodByFoodNo(food_no));
-//	}
+//   @RequestMapping("/detailFood")  포기... crud가 너무 힘들다
+//   public void detailFood(HttpServletRequest request,Model model,int c_no ,int food_no) {
+//      String path = request.getRealPath("/courseLine")+"/";
+//      model.addAttribute("c", cdao.getCourseByCno(c_no, path));
+//      model.addAttribute("f", cdao.getFoodByFoodNo(food_no));
+//   }
 }
--- oraFinal/src/main/webapp/WEB-INF/views/myPageSaveCourse.jsp
+++ oraFinal/src/main/webapp/WEB-INF/views/myPageSaveCourse.jsp
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html lang="ko">
 
@@ -10,15 +9,17 @@
    <style>
    ul { //메뉴 스타일
         box-pack:center;
-                       justify-content: center; // 창에따라 사이즈 달라짐
+        justify-content: center; // 창에따라 사이즈 달라짐
         background-color: #ffffff;
         list-style-type: none;
         margin: 50px; //메뉴 위치
         padding: 50px;
+      padding-left:200px;
         overflow: hidden;
      }
-     li {
+     .mytab li {
          float: center;
+
        }
       li a {
          display: block;
@@ -71,56 +72,165 @@
               padding:5px;
               margin-top:11px;
             }
+
+        <!-- 사진 프레임-->
+           .all{
+             position:relative;
+             min-height:1px;
+             padding-top: 50px;
+           }
+
+           .photo{
+              background-color:pink;
+           }
+           .name{
+              background-color:#AFF1FB;
+           }
+           .level{
+              background-color: #9FF8BB;
+           }
+           .time{
+              background-color: #FBCA93;
+           }
+
+         .img {
+           border: 1px solid #ddd;
+           border-radius: 4px;
+           padding: 5px;
+           width: 40%;
+           max-height:100px;
+           max-width: 200px;
+         }
+
+
+
+          .zenbu{
+          border: 1px solid gold;
+          padding: 10px;
+          height: auto;
+          min-height: 130px;
+          max-width: 200px;
+          overflow: auto;
+          display: inline-block;
+          background-color: #FBB8CF;
+          }
+
+        .bookmark{
+             background: yellow;
+             width:50px;
+             height:30px;
+
+             border:none;
+          }
+
+         .syasin{
+           width: 100%;
+           max-width: 760px;
+           border-radius: 7px;
+         }
+
+
+
+
+
    </style>
 
 </head>
-
-<body>
-   <ul>
-     <h1>마이페이지</h1>
-           <li><a  href="/myPage">정보 수정</a></li>
-            <li><a class="current" href="/myPageSaveCourse">찜 목록</a></li>
-            <li><a href="/myPageMyCourse">작성 코스(준비중)</a></li>
-            <li><a href="/myPageListReview">작성 후기</a></li>
-            <li><a href="myPageListMeeting">작성 번개</a></li>
-            <li><a href="/myPageMyRank">랭킹</a></li>
-      </ul>
-<ul>
-
-
-  <h1>찜코스목록</h1>
- 
-        
-        
-<table border="1">
-
-<c:forEach var="c" items="${courseList }">
-
-   <div id="container">
-   
-      <figure>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script type="text/javascript">
+   window.onload = function(){
       
-      <c:forEach var="cp" items="${c.c_photo }" end="0">
       
-         <a href="detailCourse?c_no=${c.c_no}"><img src="${cp.cp_path }/${cp.cp_name }"></a> 
+      
+      $.ajax({
+         url: "/myPageSaveCourse",
+         method:"POST",
+         success: function(courseVo){
+            $(courseVo).each(function(i, savecoursVo) {
+               
+               let div = $("<div></div>").prop("class", "zenbu");      //전체
+				
+               let button = $("<button></button>").attr({"class":"deleteSaveCourse bookmark","value": savecoursVo.c_no}).text("삭제");
+               
+               let div2 = $("<div></div>").prop("class", "name");
+               let div2_p =$("<p></p>");
+               let div2_h5 = $("<h5></h5>");
+               let div2_span = $("<span></span>").html(savecoursVo.c_name); //코스명
+               $(div2_h5).append(div2_span);
+               $(div2).append(div2_p,div2_h5);
+               
+               let div3 =$("<div></div>").prop("class", "time"); 
+               let div3_div1=$("<div></div>").text("Level "+savecoursVo.c_difficulty);//난이도
+               let div3_div2=$("<div></div>");
+               let div3_div2_strong=$("<strong></strong>").text(savecoursVo.c_distance+"km"); //걸
+               $(div3_div2).append(div3_div2_strong);
+               $(div3).append(div3_div1,div3_div2);
+               let img=$("<img/>");
+               
+               $(savecoursVo.c_photo).each(function(i, c_photo) {
+                  img.attr({"class": "syasin" ,src :c_photo.cp_path+"/"+c_photo.cp_name});
+                  
+               });//두번째포문
+               
+                  $(div).append(img,button,div2,div3);
+                  //$("body").append(div);
+                  $("#aa").append(div);
+                  
+            });//첫번째포문
+            }//에이작 석세스
+         });//에이작끝
+
          
-           <figcaption>${c.c_no }. ${c.c_name }</figcaption>
-           
-       </c:forEach>    
+         $(document).on("click",".deleteSaveCourse", function() {
+            var data=$(this).val();
+               console.log(data);
+               $.ajax({
+                  url:"/deleteSaveCourse",
+                  method:"POST",
+                  data:{cno:data},
+                  success: function(re){
+                     alert("찜코스가 삭제되었습니다"+re);
+                     window.location.reload();
+               }});
+               
+         }); 
+   }
+</script>
+<body>
+    <ul>
+     <h1>마이페이지</h1>
+            <li class="mytab"><a href="/myPage">정보 수정</a></li>
+            <li class="mytab"><a class="current" href="/myPageSaveCourse">찜 목록</a></li>
+            <li class="mytab"><a href="/myPageMyCourse">작성 코스(준비중)</a></li>
+            <li class="mytab"><a href="/myPageListReview">작성 후기</a></li>
+            <li class="mytab"><a href="myPageListMeeting">작성 번개</a></li>
+            <li class="mytab"><a href="/myPageMyRank">랭킹</a></li>
+      </ul>
+
+<ul id="aa">
+     <h1>찜코스목록</h1>
        
-      </figure>
-   </div>
+          <div class="zenbu">
+           class= zenbu
 
-</c:forEach>
+           <img class="syasin" src="https://www.durunubi.kr/editImgUp.do?filePath=/data/koreamobility/file/2020/09/8fbc53acde254d3caf9e8fd48254fb1c.jpg" >
+               <button class="bookmark"  type="button" onclick="alert('선택한 코스를 지우시겠습니까?');" > 클릭</button>
+                     <div class="name">
+                          <p class=""> class=name  </p>
+                          <h5><span>제주 자전거길 (우도일주)</span></h5>
+                      </div>
+
+                                            <div class="time">
+                          <div class="level">class= level 12kmㆍ90분</div>
+                          <div class="a6"><strong> class=a6 쉬움</strong></div>
+                      </div>
+
+          </div>
+
+</ul> 
+<button id="add">더보기</button>
 
 
-</table>
-       
-           
-            
-
-
-</ul>
 </body>
 
 </html>
